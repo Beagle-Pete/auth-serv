6eb78e6caa9fc06d5e42419460ed418ecc645ef3
Peter Pena <serv.github@peterpena.com>
2026-01-20 20:10:56 -0500

    Sprint 1: Update signup route and test
    - API signup endpoint now required a json in body.
    - Add serde, serde-json, and uuid as dependencies
    - Update test to send a json in the request body. Currently only testing for a http code 422

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,21 @@
-use crate::helpers::TestApp;
+use crate::helpers::{TestApp, get_random_email};
 
 #[tokio::test]
-async fn root_returns_signup_ui() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_signup(&test_case).await;
+        assert_eq!(response.status().as_u16(), 422, "Failed for input: {:?}", test_case);
+    }
+
 }
