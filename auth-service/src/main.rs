b55d1aec7c6c085923b57de5b80042115a6c07d6
Peter Pena <serv.github@peterpena.com>
2026-01-20 20:40:16 -0500

    Sprint 1:
    Trivial change. Test github action

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -5,5 +5,5 @@ async fn main() {
     let app = Application::build("0.0.0.0:3000")
         .await.expect("Failed to build app");
 
-    app.run().await.expect("Failed to run app!!");
+    app.run().await.expect("Failed to run app!");
 }
