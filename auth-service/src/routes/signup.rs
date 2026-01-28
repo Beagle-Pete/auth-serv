4bc150f43dcbda08ec7bcaba3d1d04a24c056c7f
Peter Pena <serv.github@peterpena.com>
2026-01-28 00:37:26 -0500

    Sprint 2: Update sign-up routes
    - Sign-up route now add users to shared app state.
    - Add sign-up test to test for successful HTTP code 201
    - Function new in crate::domain::User made public

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,8 +1,19 @@
-use axum::{Json, response::IntoResponse, http::status::StatusCode};
-use serde::Deserialize;
+use axum::{Json, response::IntoResponse, http::status::StatusCode, extract::State};
+use serde::{Deserialize, Serialize};
 
-pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::app_state::AppState;
+use crate::domain::User;
+
+pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupRequest>) -> impl IntoResponse {
+    let user = User::new(request.email, request.password, request.requires_2fa);
+    let mut user_store = state.user_store.write().await;
+    user_store.add_user(user).unwrap();
+
+    let response = Json(SignupRespose {
+        message: "User created successfully!".to_owned(),
+    });
+
+    (StatusCode::CREATED, response)
 }
 
 #[derive(Deserialize)]
@@ -11,4 +22,9 @@ pub struct SignupRequest {
     pub password: String,
     #[serde(rename = "requires2FA")]
     pub requires_2fa: bool,
+}
+
+#[derive(Serialize)]
+pub struct SignupRespose {
+    pub message: String,
 }
