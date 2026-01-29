90ade1a8bb4a5755192d94b1a5eccec0dae64974
Peter Pena <serv.github@peterpena.com>
2026-01-28 20:37:41 -0500

    Sprint 2: Add error module
    Add error module to model the possible error cases the API may encounter.
    Implemented IntoResponse for enum which maps variants to a specific HTTP code and message

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -5,10 +5,12 @@ pub mod app_state;
 
 use routes as api_routes;
 use app_state::AppState;
+use domain::AuthAPIError;
+use serde::{Deserialize, Serialize};
 
 use std::error::Error;
 
-use axum::{Router, routing::post, serve::Serve};
+use axum::{Json, Router, http::status::StatusCode, response::{IntoResponse, Response}, routing::post, serve::Serve};
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
@@ -47,5 +49,26 @@ impl Application {
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErroMessage {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => (StatusCode::INTERNAL_SERVER_ERROR, "Unexpetected error"),
+        };
+
+        let body = Json(ErroMessage {
+            error: message.to_string(),
+        });
+
+        (status, body).into_response()
     }
 }
