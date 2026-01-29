9559cc2a8b3ce3dbb1e7eaf986818991b402a1bb
Peter Pena <serv.github@peterpena.com>
2026-01-29 00:17:37 -0500

    Sprint 2: Add signup router errors and tests

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -2,18 +2,36 @@ use axum::{Json, response::IntoResponse, http::status::StatusCode, extract::Stat
 use serde::{Deserialize, Serialize};
 
 use crate::app_state::AppState;
-use crate::domain::User;
+use crate::domain::{AuthAPIError, User};
+use crate::services::hashmap_user_store::UserStoreError;
 
-pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupRequest>) -> impl IntoResponse {
-    let user = User::new(request.email, request.password, request.requires_2fa);
+pub async fn signup(State(state): State<AppState>, Json(request): Json<SignupRequest>) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
+    let requires_2fa = request.requires_2fa;
+
+    if !email.contains("@") || password.len() < 8 {
+        return Err(AuthAPIError::InvalidCredentials);
+    }
+
+    let user = User::new(email, password, requires_2fa);
     let mut user_store = state.user_store.write().await;
-    user_store.add_user(user).unwrap();
+
+    user_store.add_user(user)
+        .map_err(|err| 
+            {
+                match err {
+                    UserStoreError::UserAlreadyExists => AuthAPIError::UserAlreadyExists,
+                    _ => AuthAPIError::UnexpectedError,
+                }
+            }
+        )?;
 
     let response = Json(SignupRespose {
         message: "User created successfully!".to_owned(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
 
 #[derive(Deserialize)]
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,3 +1,5 @@
+use auth_service::ErrorResponse;
+
 use crate::helpers::{TestApp, get_random_email};
 
 #[tokio::test]
@@ -20,6 +22,68 @@ async fn should_return_201_if_valid_input() {
 }
 
 #[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "email": "exampleatexample.com",
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "pass",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_signup(&test_case).await;
+        
+        assert_eq!(response.status().as_u16(), 400, "Failed for input: {:?}", test_case);
+
+        assert_eq!(
+            response.json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialized response body to ErrorResponse")
+                .error, 
+            "Invalid credentials".to_owned()
+        )
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    let app = TestApp::new().await;
+    let random_email = get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases {
+        app.post_signup(&test_case).await;
+        let response = app.post_signup(&test_case).await;
+        
+        assert_eq!(response.status().as_u16(), 409, "Failed for input: {:?}", test_case);
+
+        assert_eq!(
+            response.json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialized response body to ErrorResponse")
+                .error, 
+            "User already exists".to_owned()
+        )
+    }
+}
+
+#[tokio::test]
 async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
@@ -34,5 +98,4 @@ async fn should_return_422_if_malformed_input() {
         let response = app.post_signup(&test_case).await;
         assert_eq!(response.status().as_u16(), 422, "Failed for input: {:?}", test_case);
     }
-
 }
