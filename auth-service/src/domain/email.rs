5b002b33ffa1b982cb9b50853a09f13d28c1138f
Peter Pena <serv.github@peterpena.com>
2026-01-29 17:15:38 -0500

    Sprint 2: Add tests for Email type

--- auth-service/src/domain/email.rs
+++ auth-service/src/domain/email.rs
@@ -1,5 +1,6 @@
 use super::AuthAPIError;
 
+#[derive(Debug, Clone, PartialEq)]
 struct Email (String);
 
 impl AsRef<str> for Email {
@@ -17,5 +18,37 @@ impl Email {
         }
 
         Ok(Self(email))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_valid_input() {
+        let email = Email::parse("test@example.com".to_owned()).unwrap();
+
+        assert_eq!(email.0, "test@example.com".to_owned());
+    }
+
+    #[tokio::test]
+    async fn test_asref_impl() {
+        let email = Email::parse("test@example.com".to_owned()).unwrap();
+        let email = email.as_ref();
+
+        assert_eq!(email, "test@example.com");
+    }
+
+    #[tokio::test]
+    async fn test_invalid_input() {
+
+        let test_cases = [
+            Email::parse("testexample.com".to_owned()),
+        ];
+
+        for test_case in test_cases {
+            assert_eq!(test_case, Err(AuthAPIError::InvalidCredentials));
+        }
     }
 }
