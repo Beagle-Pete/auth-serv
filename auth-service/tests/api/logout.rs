fef02286471c8f81a3f493f1b0e62a49dc6c556b
Peter Pena <serv.github@peterpena.com>
2026-02-07 09:41:20 -0500

    Sprint 3: Add test for logout API route
    - New code 400 test added to check if no cookies exists
    - Update "should_return_400_if_jwt_cookie_missing". Add a random cookie and then check if JWT cookie exists

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -5,15 +5,32 @@ use reqwest::{Url, cookie::CookieStore};
 use crate::helpers::{TestApp, parse_cookie_values};
 
 #[tokio::test]
-async fn should_return_400_if_jwt_cookie_missing() {
-    // TODO: This only accounts for when there are no cookies, but not when there are cookies but the JWT is missing
-    // Add logic to check if JWT is missing
+async fn should_return_400_if_there_are_no_cookies() {
     let app = TestApp::new().await;
 
     let url = &Url::parse("http://127.0.0.1").expect("Failed to parse URL");
     let cookies = app.cookie_jar.cookies(url);
     
     assert!(cookies.is_none())
+}
+
+#[tokio::test]
+async fn should_return_400_if_jwt_cookie_missing() {
+    let app = TestApp::new().await;
+
+    let url = &Url::parse("http://127.0.0.1").expect("Failed to parse URL");
+    // add a cookie
+    app.cookie_jar.add_cookie_str(
+        "random=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+        url,
+    );
+    
+    let cookies = app.cookie_jar.cookies(url).unwrap();
+    let cookies = parse_cookie_values(cookies.to_str().unwrap());
+
+    let cookie_exists = cookies.contains_key(JWT_COOKIE_NAME);
+    
+    assert!(!cookie_exists)
 }
 
 #[tokio::test]
