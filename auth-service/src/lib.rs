03897fc6a1a51453c14fd7878b9c4c2855b9d040
Peter Pena <serv.github@peterpena.com>
2026-02-08 00:37:50 -0500

    Sprint 3: Add CORS configuration
    - CORS added to allow app-service to communicate with auth-service
        - Configure to allow app-service from localhost or Droplet
        - Only GET and POST methods allowed.
        - Use "cors" feature from "tower-http" dependency
    - Set Droplet IP address as environmental variable
        - Update compose.yml to set new "DROPLET_IP" env var
    - Update Dockerfile to use rust:1.88-alpine
        - Resolve build error where rust:1.85-alpine did not support time@0.3.46, time-core@0.1.8, time-core@0.1.8, time-macros@0.2.26
        - Issue went unnotice because shell script docker.sh would run older images if build failed

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -6,12 +6,13 @@ pub mod utils;
 
 use routes as api_routes;
 use app_state::AppState;
+use utils::constants::{DROPLET_IP, JWT_SECRET};
 
 use std::error::Error;
 
-use axum::{Router, routing::post, serve::Serve};
+use axum::{Router, routing::post, serve::Serve, http::Method};
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
+use tower_http::{services::ServeDir, cors::CorsLayer};
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -24,6 +25,17 @@ pub struct Application {
 impl Application {
     pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
         let assets_dir = ServeDir::new("assets");
+
+        let allowed_origins = [
+            "http://localhost:8001".parse()?,
+            format!("http://{}:8001", DROPLET_IP.as_str()).parse()?,
+        ];
+
+        let cors = CorsLayer::new()
+            .allow_methods([Method::GET, Method::POST])
+            .allow_credentials(true)
+            .allow_origin(allowed_origins);
+
         let router = Router::new()
             .fallback_service(assets_dir)
             .route("/signup", post(api_routes::signup))
@@ -31,7 +43,8 @@ impl Application {
             .route("/logout", post(api_routes::logout))
             .route("/verify-2fa", post(api_routes::verify_2fa))
             .route("/verify-token", post(api_routes::verify_token))
-            .with_state(app_state);
+            .with_state(app_state)
+            .layer(cors);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -3,20 +3,23 @@ use lazy_static::lazy_static;
 use std::env as std_env;
 
 lazy_static! {
-    pub static ref JWT_SECRET: String = set_token();
+    pub static ref JWT_SECRET: String = set_token(env::JWT_SECRET_ENV_VAR);
+    pub static ref DROPLET_IP: String = set_token(env::DROPLET_IP_ENV_VAR);
 }
 
-fn set_token() -> String {
+fn set_token(var_name: &str) -> String {
     dotenv().ok();
-    let secret = std_env::var(env::JWT_SECRET_ENV_VAR).expect("JWT_SECRET must be set.");
+    let secret = std_env::var(var_name).unwrap_or_else(|_| panic!("{} must be set.", var_name));
     if secret.is_empty() {
-        panic!("JWT_SECRET must not be empty.");
+        panic!("{var_name} must not be empty.");
     }
+    println!("env var - name: {var_name}, value: {secret}");
     secret
 }
 
 pub mod env {
     pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
+    pub const DROPLET_IP_ENV_VAR: &str = "DROPLET_IP";
 }
 
 pub const JWT_COOKIE_NAME: &str = "jwt";
