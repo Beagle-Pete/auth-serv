27acf7e4e6e5a8867cbbbfbe12c86b9f8920683d
Peter Pena <serv.github@peterpena.com>
2026-02-09 22:21:29 -0500

    Sprint 3: Create banned token store
    - Create trait BannedTokenStore and struct HashsetBannedTokenStore
    - BannedTokenStore implemented on HashsetBannedTokenStore

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -8,6 +8,11 @@ pub enum UserStoreError {
     UnexpectedError,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum BannedTokenStoreError {
+    UnexpectedError,
+}
+
 #[async_trait::async_trait]
 pub trait UserStore: Send + Sync {
     async fn add_user(&mut self, user: User) -> Result<(), UserStoreError>;
@@ -15,4 +20,11 @@ pub trait UserStore: Send + Sync {
     async fn get_user(&self, email: &Email) -> Result<&User, UserStoreError>;
     
     async fn validate_user(&self, email: &Email, password: &Password) -> Result<(), UserStoreError>;
+}
+
+#[async_trait::async_trait]
+pub trait BannedTokenStore: Send + Sync {
+    async fn store_token(&mut self, token: &String) -> Result<(), BannedTokenStoreError>;
+
+    async fn check(&self, token: &String) -> Result<bool, BannedTokenStoreError>;
 }
---
+++ auth-service/src/services/hashset_banned_token_store.rs
@@ -0,0 +1,67 @@
+use crate::domain::{BannedTokenStore, BannedTokenStoreError};
+
+use std::collections::HashSet;
+
+#[derive(Default)]
+pub struct HashsetBannedTokenStore {
+    tokens: HashSet<String>,
+}
+
+#[async_trait::async_trait]
+impl BannedTokenStore for HashsetBannedTokenStore {
+    async fn store_token(&mut self, token: &String) -> Result<(), BannedTokenStoreError> {
+        self.tokens.insert(token.into());
+
+        Ok(())
+    }
+
+    async fn check(&self, token: &String) -> Result<bool, BannedTokenStoreError> {
+        
+        Ok(self.tokens.contains(token))
+    }   
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_add_token() {
+        let mut banned_tokens = HashsetBannedTokenStore::default();
+
+        let token1 = "token1".to_owned();
+        let token2 = "token2".to_owned();
+
+        banned_tokens.store_token(&token1).await.unwrap();
+        banned_tokens.store_token(&token2).await.unwrap();
+        banned_tokens.store_token(&token2).await.unwrap();
+
+        assert!(banned_tokens.tokens.contains(&token1));
+        assert!(banned_tokens.tokens.contains(&token2));
+    }
+
+    #[tokio::test]
+    async fn test_get_existing_token() {
+        let mut banned_tokens = HashsetBannedTokenStore::default();
+
+        let token1 = "token1".to_owned();
+        let token2 = "".to_owned();
+
+        banned_tokens.store_token(&token1).await.unwrap();
+        banned_tokens.store_token(&token2).await.unwrap();
+
+        assert!(banned_tokens.check(&token1).await.unwrap());
+        assert!(banned_tokens.check(&token2).await.unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_get_nonexisting_token() {
+        let mut banned_tokens = HashsetBannedTokenStore::default();
+
+        let token1 = "token1".to_owned();
+        let token2 = "".to_owned();
+
+        assert!(!banned_tokens.check(&token1).await.unwrap());
+        assert!(!banned_tokens.check(&token2).await.unwrap());
+    }
+}
