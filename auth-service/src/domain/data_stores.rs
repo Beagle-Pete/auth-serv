34c5770ba533792491aa5985244f5201cb853f23
Peter Pena <serv.github@peterpena.com>
2026-02-12 00:45:14 -0500

    Sprint 4: Create 2FA code store trait
    Create 2FA code store trait and structs to parse login attemp ID and 2FA codes.
    
    - Create 2FA code store trait. Requires following three functions to be implemented
        - add_code
        - remove_code
        - get_code
    - Create tuple structs LoginAttemptId and TwoFACode
        - Use "parse, don't validate" principle. Internal fields are private and inputs are parsed.
        - Default trait implemented.
    - `rand` dependency moved from `dev-dependencies` to `dependencies` and set to latest crate version of "0.10.0"

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -1,5 +1,7 @@
 use super::{User, Email, Password};
 
+use rand::prelude::*;
+
 #[derive(Debug, PartialEq)]
 pub enum UserStoreError {
     UserAlreadyExists,
@@ -28,4 +30,73 @@ pub trait BannedTokenStore: Send + Sync {
     async fn add_token(&mut self, token: &str) -> Result<(), BannedTokenStoreError>;
 
     async fn check_token(&self, token: &str) -> Result<bool, BannedTokenStoreError>;
+}
+
+// 2FA Store
+#[async_trait::async_trait]
+pub trait TwoFACodeStore {
+    async fn add_code(
+        &mut self,
+        email: Email,
+        login_attempt_id: LoginAttemptId,
+        code: TwoFACode,
+    ) -> Result<(), TwoFACodeStoreError>;
+
+    async fn remove_code(&mut self, email: &Email) -> Result<(), TwoFACodeStoreError>;
+
+    async fn get_code(&self, email: &Email) -> Result<(LoginAttemptId, TwoFACode), TwoFACodeStoreError>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TwoFACodeStoreError {
+    LoginAttempIdNotFound,
+    UnexpectedError,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LoginAttemptId(String);
+
+impl LoginAttemptId {
+    pub fn parse(id: String) -> Result<Self, String> {
+        if uuid::Uuid::try_parse(&id).is_err() {
+            return Err("Login Attempt ID not a valid".to_owned());
+        }
+
+        Ok(LoginAttemptId(id))
+    }
+}
+
+impl Default for LoginAttemptId {
+    fn default() -> Self {
+        LoginAttemptId(uuid::Uuid::new_v4().into())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TwoFACode(String);
+
+impl TwoFACode {
+    pub fn parse(code: String) -> Result<Self, String> {
+        match code.parse::<usize>() {
+            Ok(_) => {
+                if code.len() != 6 {
+                    return Err("2FA code must be 6 digits".to_owned());
+                }
+
+                Ok(TwoFACode(code))
+            },
+            Err(_) => Err("Code ".to_owned()),
+        }        
+    }
+}
+
+impl Default for TwoFACode {
+    fn default() -> Self {
+        let code = rand::rng().random_range(0..=999_999).to_string();
+        let leading_zeros = "0".repeat(6-code.len());
+        
+        let code = format!("{}{}", leading_zeros, code);
+
+        TwoFACode(code)
+    }
 }
