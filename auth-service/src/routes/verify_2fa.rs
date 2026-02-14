b88b71eeea6cf46442d48b7a1a32c62690397396
Peter Pena <serv.github@peterpena.com>
2026-02-14 02:15:56 -0500

    Sprint 4: Remove 2FA code from store
    - 2FA code is removed from store when verify-2fa is successful
    - Add new test for HTTP 401 to verify that a 2FA code can't be used more than once
    - Place 2FA code store read and write instances into a code block so that it's dropped right after it's done, freeing up the store for other threads and fast as possible.

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -2,15 +2,18 @@ use axum::response::IntoResponse;
 use axum::http::status::StatusCode;
 use axum::Json;
 use axum::extract::State;
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 use crate::domain::{AuthAPIError, Email, LoginAttemptId, TwoFACode};
 use crate::AppState;
+use crate::utils::auth;
 
 pub async fn verify_2fa(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<VerifyTwoFARequest>
-) -> Result<impl IntoResponse, AuthAPIError> {
+) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
 
     // Verify request by parsing. If not valid return HTTP code 400
     let email = Email::parse(request.email)?;
@@ -18,16 +21,32 @@ pub async fn verify_2fa(
     let two_fa_code = TwoFACode::parse(request.two_fa_code)?;
 
     // Verify login attempt ID and 2FA code Are correct. If not valid return HTTP code 401
-    let two_fa_store = state.two_fa_code_store.read().await;
-    let (login_attempt_id_true, two_fa_code_true) = two_fa_store.get_code(&email)
-        .await
-        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
+    let (login_attempt_id_true, two_fa_code_true) = {
+        let two_fa_store = state.two_fa_code_store.read().await;
+        let (login_attempt_id_true, two_fa_code_true) = two_fa_store.get_code(&email)
+            .await
+            .map_err(|_| AuthAPIError::IncorrectCredentials)?;
+        (login_attempt_id_true, two_fa_code_true)
+    };
 
     if (login_attempt_id != login_attempt_id_true) || (two_fa_code != two_fa_code_true) {
         return Err(AuthAPIError::IncorrectCredentials);
     }
 
-    Ok(StatusCode::OK)
+    // Create JWT token
+    let auth_cookie = auth::generate_auth_cookie(&email)
+        .map_err(|_| {AuthAPIError::UnexpectedError})?;
+    let updated_jar = jar.add(auth_cookie);
+
+    // Remove 2FA code from store
+    {
+        let mut two_fa_store = state.two_fa_code_store.write().await;
+        two_fa_store.remove_code(&email)
+            .await
+            .map_err(|_| AuthAPIError::UnexpectedError)?;
+    }
+
+    Ok((updated_jar, StatusCode::OK))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
--- auth-service/tests/api/verify_2fa.rs
+++ auth-service/tests/api/verify_2fa.rs
@@ -1,6 +1,62 @@
-use auth_service::domain::{Email, LoginAttemptId, TwoFACode};
+use auth_service::{domain::{Email, LoginAttemptId, TwoFACode}, utils::constants::JWT_COOKIE_NAME};
 
-use crate::helpers::{TestApp, get_random_email};
+use crate::helpers::{TestApp, get_all_cookies, get_random_email};
+
+#[tokio::test]
+async fn should_return_200_if_correct_code() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+    let email = Email::parse(random_email.clone()).unwrap();
+
+    // Signup 
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    // Login
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
+
+    assert_eq!(response.status().as_u16(), 206);
+
+    // Verify 2FA
+    let two_fa_code_store = app.two_fa_code_store.read().await;
+    let (login_attempt_id, two_fa_code) = two_fa_code_store.get_code(&email).await.unwrap();
+    drop(two_fa_code_store);
+
+    let verify_body = serde_json::json!({
+            "email": random_email,
+            "loginAttemptId": login_attempt_id.as_ref(),
+            "2FACode": two_fa_code.as_ref(),
+    });
+
+    let response = app.post_verify_2fa(&verify_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    // Verify JWT token was created
+    let cookies = get_all_cookies(&response);
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    let verify_token_body = serde_json::json!({
+        "token": cookies.get(JWT_COOKIE_NAME).unwrap(),
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
 
 #[tokio::test]
 async fn should_return_400_if_invalid_input() {
@@ -96,7 +152,6 @@ async fn should_return_401_if_incorrect_credentials() {
 
 #[tokio::test]
 async fn should_return_401_if_old_code() {
-    // Call login twice. Then, attempt to call verify-fa with the 2FA code from the first login requet. This should fail. 
     let app = TestApp::new().await;
 
     let random_email = get_random_email();
@@ -145,6 +200,66 @@ async fn should_return_401_if_old_code() {
             "2FACode": two_fa_code_one.as_ref(),
     });
 
+    let response = app.post_verify_2fa(&verify_body).await;
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[tokio::test]
+async fn should_return_401_if_same_code_twice() {  
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+    let email = Email::parse(random_email.clone()).unwrap();
+
+    // Signup 
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    // Login
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
+
+    assert_eq!(response.status().as_u16(), 206);
+
+    // Verify 2FA
+    let two_fa_code_store = app.two_fa_code_store.read().await;
+    let (login_attempt_id, two_fa_code) = two_fa_code_store.get_code(&email).await.unwrap();
+    drop(two_fa_code_store);
+
+    let verify_body = serde_json::json!({
+            "email": random_email,
+            "loginAttemptId": login_attempt_id.as_ref(),
+            "2FACode": two_fa_code.as_ref(),
+    });
+
+    let response = app.post_verify_2fa(&verify_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    // Verify JWT token was created
+    let cookies = get_all_cookies(&response);
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    let verify_token_body = serde_json::json!({
+        "token": cookies.get(JWT_COOKIE_NAME).unwrap(),
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+
+    assert_eq!(response.status().as_u16(), 200);  
+
+    // Verify 2FA again
     let response = app.post_verify_2fa(&verify_body).await;
     assert_eq!(response.status().as_u16(), 401);
 }
