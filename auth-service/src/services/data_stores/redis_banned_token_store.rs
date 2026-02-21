9a5964a7d5eb25e1daed91d6db8198dbed6685f4
Peter Pena <serv.github@peterpena.com>
2026-02-21 16:15:39 -0500

    Sprint 5: Remove comments

--- auth-service/src/services/data_stores/redis_banned_token_store.rs
+++ auth-service/src/services/data_stores/redis_banned_token_store.rs
@@ -24,13 +24,6 @@ impl RedisBannedTokenStore {
 #[async_trait::async_trait]
 impl BannedTokenStore for RedisBannedTokenStore {
     async fn add_token(&mut self, token: &str) -> Result<(), BannedTokenStoreError> {
-        // TODO:
-        // 1. Create a new key using the get_key helper function.
-        // 2. Call the set_ex command on the Redis connection to set a new key/value pair with an expiration time (TTL). 
-        // The value should simply be a `true` (boolean value).
-        // The expiration time should be set to TOKEN_TTL_SECONDS.
-        // NOTE: The TTL is expected to be a u64 so you will have to cast TOKEN_TTL_SECONDS to a u64. 
-        // Return BannedTokenStoreError::UnexpectedError if casting fails or the call to set_ex fails.
         let key = get_key(token);
 
         let mut conn_lock = self.conn.write().await;
@@ -40,7 +33,6 @@ impl BannedTokenStore for RedisBannedTokenStore {
     }
 
     async fn check_token(&self, token: &str) -> Result<bool, BannedTokenStoreError> {
-        // Check if the token exists by calling the exists method on the Redis connection
         let key = get_key(token);
 
         let mut conn_lock = self.conn.write().await;
